d12a69298830d161fedd976eccf49ed89d5ffc22
Zsezsu <zsezsu6@gmail.com>
2022-01-17 19:35:48 +0100

    Init: server setup

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, url_for, redirect, session, escape
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(
+        debug=True,
+        port=8000,
+        host='0.0.0.0'
+    )
